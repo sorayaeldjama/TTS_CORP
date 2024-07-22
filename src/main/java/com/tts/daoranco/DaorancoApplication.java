4942bc5dea0910c7094d668a78fcefb9cf1a8a19
soraya <sorayaeldjama@gmail.com>
2024-07-22 15:04:15 +0200

    first commit

---
+++ src/main/java/com/tts/daoranco/DaorancoApplication.java
@@ -0,0 +1,13 @@
+package com.tts.daoranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaorancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaorancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/daoranco/DaorancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.daoranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaorancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
